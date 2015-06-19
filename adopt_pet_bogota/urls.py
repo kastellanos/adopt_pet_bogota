237fce4a30a57c99ee30b32ad4461c9aaf9e2b94
kastellanos <castellanos1458@hotmail.com>
2015-06-18 23:19:54 -0500

    Fixing usses

--- adopt_pet_bogota/urls.py
+++ adopt_pet_bogota/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 admin.autodiscover()
 
-import hello.views
+import ada.views
 
 urlpatterns = patterns('',
     # Examples:
