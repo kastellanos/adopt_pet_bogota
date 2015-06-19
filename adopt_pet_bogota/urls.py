ea817e49e08a1e78444747d09852bf815d2909d1
kastellanos <castellanos1458@hotmail.com>
2015-06-18 23:16:14 -0500

    Fixing usses

--- adopt_pet_bogota/urls.py
+++ adopt_pet_bogota/urls.py
@@ -1,10 +1,16 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+admin.autodiscover()
 
-urlpatterns = [
+import hello.views
+
+urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'adopt_pet_bogota.views.home', name='home'),
+    # url(r'^$', 'gettingstarted.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^$', hello.views.index, name='index'),
+    url(r'^db', hello.views.db, name='db'),
     url(r'^admin/', include(admin.site.urls)),
-]
+
+)
